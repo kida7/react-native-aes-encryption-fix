1b21f018be9708835808f2a1cdacdccac20a3266
Vinh Le <kida7kahp@gmail.com>
2017-10-19 09:06:04 +0700

    1

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import React, { NativeModules } from 'react-native';
-module.exports = NativeModules.ReactAES;
+var aes = NativeModules.ReactAES;
+var base64 = require('base-64')
+
+export async function encrypt(data, key, iv){
+    return await aes.encrypt(data,key, base64.encode(iv));
+}
+
+export async function decrypt(data, key, iv){
+    return await aes.decrypt(base64.encode(data),key, base64.encode(iv));
+}
+
+export async function generateRandomIV(){
+    return await aes.generateRandomIV();
+}
